31915b3a44c01fa3812a3c7bfb0fb6861e87d514
Shamarni <Smcfarlane926@gmail.com>
2020-11-06 15:57:36 +0000

    completed challenge 13

--- app.rb
+++ app.rb
@@ -27,5 +27,17 @@ class BookmarkManager < Sinatra::Base
     redirect '/bookmarks'
   end
 
+  patch '/bookmarks/:id' do
+    p params
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+
+
   run! if app_file == $0
 end
--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -33,6 +33,20 @@ class Bookmark
     @con.exec("DELETE FROM bookmarks WHERE id = #{id}")
   end
 
+  def self.update(id:, url:, title:)
+    self.environment
+
+    result = @con.exec("UPDATE bookmarks SET url = '#{url}', title = '#{title}' WHERE id = #{id} RETURNING id, url, title;")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
+  def self.find(id:)
+    self.environment
+
+    result = @con.exec("SELECT * FROM bookmarks WHERE id = #{id}")
+    Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
+  end
+
   private
 
   def self.environment
